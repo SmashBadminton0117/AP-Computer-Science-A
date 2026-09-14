8eaec6c0194f496eadb9a2598344f846ed4f5ebc
Lorenzo <2108599@learn.vsb.bc.ca>
2026-09-14 13:33:37 -0700

    Add files via upload

---
+++ Unit 2 - Variables and Operators/Unit 2 - Excercise set/Time.java
@@ -0,0 +1,15 @@
+public class Time {
+
+	public static void main(String[] args) {
+        
+        //declarations
+        int hour = 13;
+        int minute = 28;
+        int second = 03;
+        
+        //method
+        System.out.print(hour + ":");
+        System.out.print(minute + ":");
+        System.out.print("0" + second);
+    }
+}
